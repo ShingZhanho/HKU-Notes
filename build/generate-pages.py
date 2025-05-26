92beb35c53155d1ab0a639e0f49205085e4be3f3
Jacob Shing <zhanhoshing@icloud.com>
2025-05-26 16:32:03 +0800

    feat: include document status in details page generation

--- build/generate-pages.py
+++ build/generate-pages.py
@@ -53,6 +53,7 @@ def main():
             print(f"Generating details page for {target}")
             f.write(f"# {target}\n\n")
             f.write(f"File description: {metadata.static_site__description}\n\n")
+            f.write(f"Document status: {generate_badge(metadata.static_site__document_status)}\n\n")
             f.write(f"??? \"Digital Digest\"\n\n")
             f.write(f"\tSource file hash:`{src_checksum}`\n\n")
             f.write(f"\tCompiled at:`{compiled_at}`\n")
