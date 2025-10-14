2648db57fb9e157396806e4367273420dd68d928
Jacob Shing <zhanhoshing@icloud.com>
2025-10-14 18:39:53 +0800

    fix: splide settings overriden by mkdocs

--- site/docs/javascripts/splide_mount.js
+++ site/docs/javascripts/splide_mount.js
@@ -4,9 +4,6 @@ document$.subscribe(function() {
     for (var i = 0; i < splides.length; i++) {
         var splide = new Splide(splides[i], {
             type: 'slide',
-            width: '100%',
-            autoWidth: true,
-            height: '85vh',
             autoplay: false,
             trimSpace: false,
             padding: '10%',
