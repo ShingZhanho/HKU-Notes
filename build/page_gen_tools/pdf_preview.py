371e044cd11c7e5cccf73956e17bf88244d5ea36
Jacob Shing <zhanhoshing@icloud.com>
2025-11-01 11:26:38 +0800

    feat: scroll to page in preview

--- build/page_gen_tools/pdf_preview.py
+++ build/page_gen_tools/pdf_preview.py
@@ -18,7 +18,7 @@ def generate_pdf_viewer_html(target: str) -> str:
         sb.extend((
             '<div class="preview__item">',
             f'<img class="preview__image on-glb" src="./{target}~preview/{target}_preview-{pad_number(i, page_count)}.png" ',
-            f'alt="Page {i} of {target}" loading="lazy">',
+            f'alt="Page {i} of {target}" loading="lazy" id="item-{i - 1}">',
             f'<div class="preview__caption">Page {i} of {page_count}</div>',
             '</div>',
         ))
