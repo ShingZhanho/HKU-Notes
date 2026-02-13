91bc51820e4c7df1dd30d1ce6419d573dc66655f
Jacob Shing <zhanhoshing@icloud.com>
2026-02-14 00:40:42 +0800

    fix: build figures on both local and remote upon main build

--- src/ENGG1310-Past-Paper-Solutions/pre_build.sh
+++ src/ENGG1310-Past-Paper-Solutions/pre_build.sh
@@ -5,7 +5,11 @@
 FIGS_DIR="figs"
 for fig in "$FIGS_DIR"/*.tex; do
     if [ -f "$fig" ]; then
-        echo "Compiling $fig..."
-        pdflatex -output-directory="$FIGS_DIR" "$fig"
+        pdf="${fig%.tex}.pdf"
+        # Only compile if PDF doesn't exist or if .tex is newer than .pdf
+        if [ ! -f "$pdf" ] || [ "$fig" -nt "$pdf" ]; then
+            echo "Compiling $fig..."
+            pdflatex -interaction=batchmode -output-directory="$FIGS_DIR" "$fig"
+        fi
     fi
 done
